3af421f0e5e9443095690a9c1f8a9f8a16cd0e34
Juan Miñaca <jminaca@espol.edu.ec>
2025-07-30 15:52:42 -0500

    Test MakeFormula
    se realizaron los diferentes tests para el metodo 'MakeFormula'

--- TestsOperations.java
+++ TestsOperations.java
@@ -1,3 +1,27 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestsOperations {
+    
+   @Test
+   @DisplayName("Haciendo pruebas de MakeFormula")
+    public void testMakeFormula() {
+        String formula = Operations.MakeFormula();
+        @Test
+        @DisplayName("Verificar que en la formula no este vacia o sea nula")
+        assertNotNull(formula);
+        assertFalse(formula.isEmpty());
+        
+        @Test
+        @DisplayName("Se verifica que se esten usando los operadores permitidos")
+        assertTrue(formula.matches(".*[+\\-*/].*"), "La fórmula debe contener al menos un signo de operador");
+        
+        @Test 
+        @DisplayName("Se verifica que los caracteres ingresados sean validos")
+        assertTrue(formula.matches("[0-9+\\-*/]+"), "La fórmula contiene caracteres invalidos");
+        
+        System.out.println("Formula generada: " + formula);
+    }
+
     
 }
